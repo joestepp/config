e371463339ab03a05a777f4eb2a5f6c8af927ff9
Joe Stepp <joe@wave.com>
2019-01-30 18:40:27 -0800

    continue on if command fails

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 set -eo pipefail
 
 echo 'symlinking zshrc'
-ln -sf $PWD/zshrc ~/.zshrc
+ln -sf $PWD/zshrc ~/.zshrc || true
 echo 'symlinking vimrc'
-ln -sf $PWD/vimrc ~/.vimrc
+ln -sf $PWD/vimrc ~/.vimrc || true
 echo 'symlinking nvim configs to vim configs'
-ln -sf ~/.vim ~/.config/nvim
-ln -sf ~/.vimrc ~/.config/nvim/init.vim
+ln -sf ~/.vim ~/.config/nvim || true
+ln -sf ~/.vimrc ~/.config/nvim/init.vim || true
 echo 'symlinking tmux.conf'
-ln -sf $PWD/tmux.conf ~/.tmux.conf
+ln -sf $PWD/tmux.conf ~/.tmux.conf || true
 echo 'symlinking psqlrc'
-ln -sf $PWD/psqlrc ~/.psqlrc
+ln -sf $PWD/psqlrc ~/.psqlrc || true
 echo 'symlinking zimrc'
-ln -sf $PWD/zimrc ~/.zimrc
+ln -sf $PWD/zimrc ~/.zimrc || true
